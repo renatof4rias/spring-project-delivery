31622ebab51a1c913b910d26596ea5e49081a54c
renatof4rias <renatof4rias@gmail.com>
2024-01-10 18:48:05 -0300

    Entity Product

---
+++ src/main/java/com/softelse/delivery/entities/Product.java
@@ -0,0 +1,94 @@
+package com.softelse.delivery.entities;
+
+import java.util.Objects;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_product")
+public class Product {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String name;
+	private Double price;
+	private String description;
+	private String imageUri;
+	
+	public Product() {
+		
+	}
+
+	public Product(Long id, String name, Double price, String description, String imageUri) {
+		this.id = id;
+		this.name = name;
+		this.price = price;
+		this.description = description;
+		this.imageUri = imageUri;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Double getPrice() {
+		return price;
+	}
+
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getImageUri() {
+		return imageUri;
+	}
+
+	public void setImageUri(String imageUri) {
+		this.imageUri = imageUri;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(description, id, imageUri, name, price);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Product other = (Product) obj;
+		return Objects.equals(description, other.description) && Objects.equals(id, other.id)
+				&& Objects.equals(imageUri, other.imageUri) && Objects.equals(name, other.name)
+				&& Objects.equals(price, other.price);
+	}
+	
+}
