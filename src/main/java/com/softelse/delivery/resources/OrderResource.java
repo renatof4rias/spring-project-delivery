53dae754b065304852b8312f92a983f5ca10f31c
renatof4rias <renatof4rias@gmail.com>
2024-01-11 17:39:12 -0300

    POST /orders

--- src/main/java/com/softelse/delivery/resources/OrderResource.java
+++ src/main/java/com/softelse/delivery/resources/OrderResource.java
@@ -1,12 +1,16 @@
 package com.softelse.delivery.resources;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.softelse.delivery.entities.Order;
 import com.softelse.delivery.services.OrderService;
@@ -22,6 +26,13 @@ public class OrderResource {
 	public ResponseEntity<List<Order>> findAll() {
 		List<Order> result = orderService.findAll();
 		return ResponseEntity.ok().body(result);
+	}
+
+	@PostMapping
+	public ResponseEntity<Order> insert(@RequestBody Order obj) {
+		obj = orderService.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).body(obj);
 	}
 
 }
--- src/main/java/com/softelse/delivery/services/OrderService.java
+++ src/main/java/com/softelse/delivery/services/OrderService.java
@@ -19,4 +19,8 @@ public class OrderService {
 		return result;
 	}
 
+	public Order insert(Order obj) {
+		return orderRepository.save(obj);
+	}
+
 }
