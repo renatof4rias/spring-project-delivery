07dc02744c76e391ff88f6a81d41696fd02beabf
renatof4rias <renatof4rias@gmail.com>
2024-01-12 15:56:38 -0300

    updated in function OrderServices

--- src/main/java/com/softelse/delivery/resources/OrderResource.java
+++ src/main/java/com/softelse/delivery/resources/OrderResource.java
@@ -13,7 +13,6 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.softelse.delivery.dtos.OrderDTO;
-import com.softelse.delivery.entities.Order;
 import com.softelse.delivery.services.OrderService;
 
 @RestController
@@ -30,7 +29,7 @@ public class OrderResource {
 	}
 
 	@PostMapping
-	public ResponseEntity<Order> insert(@RequestBody Order obj) {
+	public ResponseEntity<OrderDTO> insert(@RequestBody OrderDTO obj) {
 		obj = orderService.insert(obj);
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
 		return ResponseEntity.created(uri).body(obj);
--- src/main/java/com/softelse/delivery/services/OrderService.java
+++ src/main/java/com/softelse/delivery/services/OrderService.java
@@ -1,14 +1,20 @@
 package com.softelse.delivery.services;
 
+import java.time.Instant;
 import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.softelse.delivery.dtos.OrderDTO;
+import com.softelse.delivery.dtos.ProductDTO;
 import com.softelse.delivery.entities.Order;
+import com.softelse.delivery.entities.Product;
+import com.softelse.delivery.enums.OrderStatus;
 import com.softelse.delivery.repositories.OrderRepository;
+import com.softelse.delivery.repositories.ProductRepository;
 
 @Service
 public class OrderService {
@@ -16,15 +22,28 @@ public class OrderService {
 	@Autowired
 	private OrderRepository orderRepository;
 
+	@Autowired
+	private ProductRepository productRepository;
+	
+	
+	@Transactional(readOnly = true)
 	public List<OrderDTO> findAll() {
 		List<Order> result = orderRepository.findOrdersWithProducts();
 		return result.stream().map(x -> new OrderDTO(x)).collect(Collectors.toList());
 	}
+	@Transactional
+	public OrderDTO insert(OrderDTO obj) {
+		Order order = new Order(null, obj.getAddress(), obj.getLatitude(), obj.getLongitude(), Instant.now(), OrderStatus.PENDING, obj.getTotal() ); 
 
-	public Order insert(Order obj) {
-		return orderRepository.save(obj);
-	}
+		for (ProductDTO p : obj.getProducts()) {
+			Product product = productRepository.getOne(p.getId());
+			order.getProducts().add(product);
+		}
+		order = orderRepository.save(order);
+		return new OrderDTO(order);
+	}  
 
 //	public void update
 	
 }
+	
